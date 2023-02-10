c57745dd5144d560b6f0217d2cd7eb140f71396d
Adriana de Lazzari <adrianaclazzari@gmail.com>
2023-02-10 14:20:05 -0500

    Adding v-select, adding countries list dropdown

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 // extract the unique country names
 
-const rawCountryList = universityJson.map(item => item.country);
-const countryList = rawCountryList.filter((item, index, self) => self.indexOf(item) === index);
+const rawCountryList = universityJson.map((item) => item.country);
+const countryList = rawCountryList.filter(
+  (item, index, self) => self.indexOf(item) === index
+);
 countryList.sort();
 
 console.log(rawCountryList);
 console.log(countryList);
+
+Vue.component("v-select", VueSelect.VueSelect);
+
+var app = new Vue({
+  el: "#vue-app",
+  data: {
+    countries: countryList,
+  },
+});
